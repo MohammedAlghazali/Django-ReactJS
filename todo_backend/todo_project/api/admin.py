de6f2b6c6813747f7fbb29d6d77e23cadef0830e
MohammedAlghazali <hamood-monzer11@hotmail.com>
2021-03-05 17:42:11 +0200

    add Todo model

--- todo_backend/todo_project/api/admin.py
+++ todo_backend/todo_project/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+
+# admin.site.register(Todo)
+
+
+@admin.register(Todo)
+class TodoModel(admin.ModelAdmin):
+    list_filter = ('title', 'description')
+    list_display = ('title', 'description')
