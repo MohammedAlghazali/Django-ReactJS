d1421e678b540c3d8f322058ab803dacc94930c9
MohammedAlghazali <hamood-monzer11@hotmail.com>
2021-03-05 22:29:41 +0200

    add needed routes for todo item GET, PUT, DELETE

--- todo_backend/todo_project/api/urls.py
+++ todo_backend/todo_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import todo_list
+from .views import todo_list, todo_details
 
 urlpatterns = [
     path('todos/', todo_list),
+    path('todos/<int:pk>/', todo_details),
 ]
--- todo_backend/todo_project/api/views.py
+++ todo_backend/todo_project/api/views.py
@@ -3,12 +3,10 @@ from .models import Todo
 from .serializers import TodoSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
-@csrf_exempt
 def todo_list(request):
     # get all todos
     if request.method == 'GET':
@@ -22,3 +20,24 @@ def todo_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+def todo_details(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return HttpResponse(status=400)
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(todo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return HttpResponse(status=204)
