c12260db8dc69ef9cf3a1eda720aaab72fde0e38
MohammedAlghazali <hamood-monzer11@hotmail.com>
2021-03-05 23:35:49 +0200

    using @api_view decorator with function based view

--- todo_backend/todo_project/api/views.py
+++ todo_backend/todo_project/api/views.py
@@ -3,41 +3,44 @@ from .models import Todo
 from .serializers import TodoSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
+@api_view(['GET', 'POST'])
 def todo_list(request):
     # get all todos
     if request.method == 'GET':
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TodoSerializer(data=data)
+        serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
 def todo_details(request, pk):
     try:
         todo = Todo.objects.get(pk=pk)
     except Todo.DoesNotExist:
-        return HttpResponse(status=400)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = TodoSerializer(todo)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = TodoSerializer(todo, data=data)
+        serializer = TodoSerializer(todo, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         todo.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
