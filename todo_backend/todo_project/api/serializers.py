7e8f9523e04082207ec818b20ddf91cfd1d1a40c
MohammedAlghazali <hamood-monzer11@hotmail.com>
2021-03-05 19:34:29 +0200

    use ModelSerializer instead of serializer

--- todo_backend/todo_project/api/serializers.py
+++ todo_backend/todo_project/api/serializers.py
@@ -2,14 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=80)
-    description = serializers.CharField(max_length=400)
-
-    def create(self, validated_data):
-        return Todo.object.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get(
-            'description', instance.description)
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'description']
